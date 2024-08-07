3005d49b49e438a01b7c4d251af2cf1c72bfb632
jibril <100243649+09jayy@users.noreply.github.com>
2024-08-07 22:05:18 +0100

    Initial commit

---
+++ Pong-Game-SDL2/main.cpp
@@ -0,0 +1,33 @@
+﻿// Pong-Game-SDL2.cpp : Defines the entry point for the application.
+//
+
+#include "main.h"
+#include "SDL.h"
+
+int main(int argsc, char* argv[])
+{
+	std::cout << "Hello CMake." << std::endl;
+    
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window* window = SDL_CreateWindow(
+        "SDL2Test",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        640,
+        480,
+        0
+    );
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+
+    SDL_Delay(3000);
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+	return 0;
+}
