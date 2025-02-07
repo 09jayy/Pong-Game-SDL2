faed0a5d570e3b23b1e883a0afab4de193e7949c
jibril <100243649+09jayy@users.noreply.github.com>
2025-02-07 01:45:00 +0000

    created collision detection logic between circular entity and rect entity

--- src/CollisionManager.cpp
+++ src/CollisionManager.cpp
@@ -1,8 +1,37 @@
 #include "CollisionManager.hpp"
+#include <cmath>
+#include <array>
 
 int CollisionManager::checkCollision(const CircleEntity auto& circle, const RectEntity auto& rect)
 {
-	
+	// 1. Find the closest edge the circle is to the rect to be used for testing
+	float testX = circle.xPos; 
+	float testY = circle.yPos; 
+	if (circle.xPos < rect.xPos)
+	{
+		testX = rect.xPos;
+	}
+	else if (circle.xPos > rect.xPos + rect.width)
+	{
+		testX = rect.xPos + rect.width; 
+	}; 
+
+	if (circle.yPos < rect.yPos)
+	{
+		testY = rect.yPos;
+	}
+	else if (circle.yPos > rect.yPos + rect.height)
+	{
+		testY = rect.yPos + rect.height;
+	}; 
+
+	// 2. Use Pythagoras Theorem to find distance between center circle and two edges
+	float distX = circle.xPos - testX; 
+	float distY = circle.yPos - testY;
+	float distance = std::sqrt((distX * distX) + (distY * distY)); 
+
+	// 3. Compare distance to circle radius
+	return (distance <= circle.radius) ? true : false; 
 };
 
 int CollisionManager::checkBorderCollision(const CircleEntity auto& circle)
